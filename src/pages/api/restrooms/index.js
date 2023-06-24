32dee12d6d928034a264e5b9b8beccddd0818c3b
keylakoizumin <kkoizuminishimura@gmail.com>
2023-06-24 00:29:14 -0700

    Implemented GET api for defaultRestroomsData on the Homepage for further development and testing
    The path is hard coded for testing. In the future the path will be dynamic according to the user's search and filter

--- src/pages/api/restrooms/index.js
+++ src/pages/api/restrooms/index.js
@@ -1,10 +1,14 @@
+/*
 export default async function getDefaultRestroomData() {
   const res = await fetch('https://www.refugerestrooms.org/api/v1/restrooms?page=1&per_page=10&offset=0');
   const data = await res.json();
-  console.log({ restrooms : data});
-  res.json({ restroom: data})
+  return {
+    props: {
+      defaultRestroomsData: "data"
+    }
+  }
 }
-
+*/
   //fetch default restroom data 
   /*
     page integer
