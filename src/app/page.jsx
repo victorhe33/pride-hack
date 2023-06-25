c480b7decf28c46b0b874b26fb4fd8dcd660ec30
keylakoizumin <kkoizuminishimura@gmail.com>
2023-06-24 17:40:23 -0700

    Create get API for fetching defaultRestroomData in home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,22 +3,22 @@ import styles from './page.module.css'
 
 
 // Victor Test Api Endpoint Call
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
 
-  return res.json()
-}
+//   return res.json()
+// }
 
 export default async function Home() {
-  const data = await getData();
+  const data = await getDefaultRestroomData();
   console.log(data);
   return (
     <main className={styles.main}>
@@ -26,4 +26,17 @@ export default async function Home() {
       <button> button </button>
     </main>
   )
+}
+
+async function getDefaultRestroomData() {
+  const res = await fetch('https://www.refugerestrooms.org/api/v1/restrooms?page=1&per_page=10&offset=0', {
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+
+  if(!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json();
 }
