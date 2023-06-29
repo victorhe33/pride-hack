53a19b57808dcbf6ac7b63844bde6ed026e96112
Victor <94798862+victorhe33@users.noreply.github.com>
2023-06-29 11:27:04 -0400

    small add to page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -7,11 +7,6 @@ import  Map from './map'
 export default function Tarik(props:any) {
   const { isLoaded } = useLoadScript({ googleMapsApiKey: String(process.env.NEXT_PUBLIC_GOOGLE_MAP_SECRET_KEY) });
   const { restrooms } = props;
-  // console.log('Tarik', restrooms)
-
-  // for (let i = 0; i < restrooms.length; i++) {
-  //   const marker = new google.maps.Marker({restrooms[i].lat, restrooms[i].lng)
-  // }
 
   if (!isLoaded) return <div>LOADING...</div>
   return <Map restrooms={restrooms.data}/>
