d3e77e23765f1d631037d332b7cedad09aacae66
keylakoizumin <kkoizuminishimura@gmail.com>
2023-07-24 21:30:29 -0700

    create restroom results page

---
+++ src/app/results/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useState, useEffect } from "react";
+
+export default async function Results() {
+  const [restrooms, setRestrooms] = useState([]);
+
+  return (
+    <h1>Restroom Finder</h1>
+
+    
+  )
+}
